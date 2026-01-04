53cb18ebd973569cbbc9cc8e9695b5e98fa24c71
Islombek Mirsalimov <mirsalimovislombek9@gmail.com>
2026-01-04 21:11:29 +0500

    alter the fibbonaci function according to the task

--- fibbonaci.js
+++ fibbonaci.js
@@ -1,13 +1,20 @@
 #!/usr/bin/env node
 
 const fibbonaci = function (n) {
-    if (n === 1 || n === 2) {
-        return 0;
+    if (n === 1) {
+        return [0];
+    } else if (n === 2) {
+        return [0, 1];
     } else {
-        return fibbonaci(n - 1) + fibbonaci(n - 2)
+        const prevArr = fibbonaci(n - 1);
+        const last = prevArr[prevArr.length - 1];
+        const oneToLast = prevArr[prevArr.length - 2];
+        const newValue = last + oneToLast;
+        prevArr.push(newValue);
+        return prevArr;
     }
 }
 
-console.log(fibbonaci(4))
+console.log(fibbonaci(8))
 
 // 0 1 1 2 3 5 8 13 21 ...s
